4435c88f7c3b28d1da7ba1fea7895df28ce9ca2a
moremi-alt <otukoyamoridiyyah@gmail.com>
2022-04-25 10:39:35 +0100

    learning Vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="app">
+    <calculator/>
+ 
+</div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+   name: 'app',
+   components: {
+        Calculator
+   }
+}
+</script>
+
+<style scoped>
+
+
+</style>
+
+
+ 
